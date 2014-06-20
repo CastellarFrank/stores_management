272557ea17257668aa197d5636cb407849da721a
= <frank_nightmare@unitec.edu>
2014-06-20 10:20:00 -0600

    adding email config to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,15 @@ StoreManagement::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      user_name:            'mhotivo@gmail.com',
+      password:             'abc098765',
+      authentication:       'plain',
+      enable_starttls_auto: true
+  }
 end
